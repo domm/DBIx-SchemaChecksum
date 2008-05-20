7b8ea4cf24259db381b124f99cda1950ef637344
Thomas Klausner <domm@cpan.org>
2008-05-20 16:36:33 +0200

    removed module-starter cruft

--- lib/DBIx/SchemaChecksum.pm
+++ lib/DBIx/SchemaChecksum.pm
@@ -2,51 +2,23 @@ package DBIx::SchemaChecksum;
 
 use warnings;
 use strict;
+use version;our $VERSION = version->new('0.01');
 
 =head1 NAME
 
-DBIx::SchemaChecksum - The great new DBIx::SchemaChecksum!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-use version;our $VERSION = version->new('0.01');
-
+DBIx::SchemaChecksum - Generate and compare checksums of database schematas
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+=head1 DESCRIPTION
 
-Perhaps a little code snippet.
-
-    use DBIx::SchemaChecksum;
-
-    my $foo = DBIx::SchemaChecksum->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
+=head1 METHODS 
 
 =cut
 
-sub function1 {
-}
+q{ favourite record of the moment: The Dynamics - Version Excursions }
 
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+__END__
 
 =head1 AUTHOR
 
@@ -54,19 +26,18 @@ Thomas Klausner, C<< <domm at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-dbix-schemachecksum at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-SchemaChecksum>.  I will be notified, and then you'll
+Please report any bugs or feature requests to 
+C<bug-dbix-schemachecksum at rt.cpan.org>, or through
+the web interface at 
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-SchemaChecksum>.  
+I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc DBIx::SchemaChecksum
-
 
 You can also look for information at:
 
@@ -90,18 +61,17 @@ L<http://search.cpan.org/dist/DBIx-SchemaChecksum>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
 
+Thanks to Klaus Ita and Armin Schreger for writing the core code. I 
+just glued it together...
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Thomas Klausner, all rights reserved.
+Copyright 2008 Thomas Klausner, revdev.at, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
 
-1; # End of DBIx::SchemaChecksum
