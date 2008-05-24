cf98464f52244a502310bcee38a8f860c7e1813c
Thomas Klausner <domm@cpan.org>
2008-05-24 10:04:32 +0200

    make t/apply_sql_snippets.t skip instead of fail if we cannot copy the testdb

--- lib/DBIx/SchemaChecksum.pm
+++ lib/DBIx/SchemaChecksum.pm
@@ -2,7 +2,7 @@ package DBIx::SchemaChecksum;
 
 use 5.010;
 use Moose;
-use version; our $VERSION = version->new('0.04');
+use version; our $VERSION = version->new('0.05');
 
 use DBI;
 use Digest::SHA1;
