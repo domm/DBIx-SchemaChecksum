3a53762335b106c77d237491de40103a3e5cb599
Klaus Ita <klaus@worstofall.com>
2008-08-21 09:51:39 +0200

    make a release

--- lib/DBIx/SchemaChecksum.pm
+++ lib/DBIx/SchemaChecksum.pm
@@ -2,7 +2,7 @@ package DBIx::SchemaChecksum;
 
 use 5.010;
 use Moose;
-use version; our $VERSION = version->new('0.07');
+use version; our $VERSION = version->new('0.08');
 
 use DBI;
 use Digest::SHA1;
