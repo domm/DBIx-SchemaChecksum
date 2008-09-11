8d95970c257a5a3b3222a1930078c2c7a3d8de42
Thomas Klausner <domm@cpan.org>
2008-09-11 11:58:31 +0200

    bumped version to 0.09

--- lib/DBIx/SchemaChecksum.pm
+++ lib/DBIx/SchemaChecksum.pm
@@ -2,7 +2,7 @@ package DBIx::SchemaChecksum;
 
 use 5.010;
 use Moose;
-use version; our $VERSION = version->new('0.08');
+use version; our $VERSION = version->new('0.09');
 
 use DBI;
 use Digest::SHA1;
