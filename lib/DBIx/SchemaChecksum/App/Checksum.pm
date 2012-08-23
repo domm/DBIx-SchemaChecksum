6c4ec5a36bcae9fc0fe6a6c35fc119076535f764
Thomas Klausner <domm@plix.at>
2012-08-23 15:26:32 +0200

    added --show_dump to checksum command

--- lib/DBIx/SchemaChecksum/App/Checksum.pm
+++ lib/DBIx/SchemaChecksum/App/Checksum.pm
@@ -3,8 +3,17 @@ use 5.010;
 use MooseX::App qw(Config);
 extends qw(DBIx::SchemaChecksum::App);
 
+option 'show_dump' => (
+    is            => 'ro',
+    isa           => 'Bool',
+    documentation => q[Show the raw database dump structure],
+    default       => 0,
+);
+
 sub run {
     my $self = shift;
+
+    say $self->scs->schemadump if $self->show_dump;
     say $self->scs->checksum;
 }
 1;
