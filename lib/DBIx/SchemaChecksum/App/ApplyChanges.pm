68e3401f916c461453ecb1666486e579499757ff
Thomas Klausner <domm@plix.at>
2012-10-05 00:16:57 +0200

    ApplyChanges: nicer prompting, lots of cleanup

--- lib/DBIx/SchemaChecksum/App/ApplyChanges.pm
+++ lib/DBIx/SchemaChecksum/App/ApplyChanges.pm
@@ -2,21 +2,17 @@ package DBIx::SchemaChecksum::App::ApplyChanges;
 use 5.010;
 use MooseX::App::Command;
 extends qw(DBIx::SchemaChecksum::App);
-use Carp qw(croak);
+use IO::Prompt::Tiny qw(prompt);
+use Try::Tiny;
 
 option '+sqlsnippetdir' => ( required => 1);
-
-# TODO: better prompting via Term::ReadKey/Line
-# TODO: remove dry_run, implement as new command 'show_update_plan' (oder so)
+has 'no_prompt'    => ( is => 'rw', isa => 'Bool', default => 0, documentation=>'Do not prompt, just use defaults');
 
 sub run {
     my $self = shift;
 
-    my $pre_checksum = $self->checksum;
-#    say "Base checksum: $pre_checksum" if $self->verbose;
-
-    $self->build_update_path( );
-    $self->apply_sql_snippets($pre_checksum);
+    $self->build_update_path;
+    $self->apply_sql_snippets($self->checksum);
 }
 
 =head3 apply_sql_snippets
@@ -30,18 +26,15 @@ rolls back the last change (if your DB supports transactions...)
 =cut
 
 sub apply_sql_snippets {
-    my $self          = shift;
-    my $this_checksum = shift;
-    croak "No current checksum" unless $this_checksum;
+    my ($self,  $this_checksum ) = @_;
     my $update_path = $self->_update_path;
 
     my $update = $update_path->{$this_checksum}
         if ( exists $update_path->{$this_checksum} );
 
     unless ($update) {
-        my $this_checksum = $self->checksum;
-        $self->dump_checksum($this_checksum) if $self->dump_checksums;
-        die "No update found that's based on $this_checksum.";
+        say "No update found that's based on $this_checksum.";
+        exit;
     }
 
     if ( $update->[0] eq 'SAME_CHECKSUM' ) {
@@ -51,48 +44,27 @@ sub apply_sql_snippets {
         $self->apply_file( $file, $expected_post_checksum );
     }
     else {
-        $self->apply_file(@$update);
+        $self->apply_file( @$update );
     }
 }
 
 sub apply_file {
     my ( $self, $file, $expected_post_checksum ) = @_;
+    my $filename = $file->relative($self->sqlsnippetdir);
 
-    if ($self->show_update_path) {
-        print $file->basename." (".$expected_post_checksum.")\n";
-        return $self->apply_sql_snippets($expected_post_checksum);
-    }
+    my $no_checksum_change = $self->checksum eq $expected_post_checksum ? 1 : 0;
 
-    my $no_checksum_change;
-    $no_checksum_change=1 if $self->checksum eq $expected_post_checksum;
-
-    my $yes = 0;
-    if ( $self->no_prompt ) {
-        $yes = 1;
-        print "Applying " .$file->basename. "\n";
+    my $answer;
+    if ($no_checksum_change) {
+        $answer = prompt("Apply $filename (won't change checksum)? [y/n/s]",'y');
     }
     else {
-        my $ask_user = 1;
-        while ($ask_user) {
-            print "Do you want me to apply <" . $file->basename . ">".($no_checksum_change ?" (won't change the checksum)" : '')."? [y/n".( $no_checksum_change ?'/s' : '')."] ";
-            my $in = <STDIN>;
-            chomp($in);
-            if ( $in =~ /^y/i ) {
-                $yes      = 1;
-                $ask_user = 0;
-            }
-            elsif ( $in =~ /^n/i ) {
-                $yes      = 0;
-                $ask_user = 0;
-            }
-            elsif ( $no_checksum_change &&  $in =~ /^s/i) {
-                return $self->apply_sql_snippets($expected_post_checksum);
-            }
-        }
+        $answer = prompt("Apply $filename? [y/n]",'y');
     }
 
-    if ($yes) {
-        say("Applying the patch") if $self->verbose;
+    if ($answer eq 'y') {
+        say "Starting to apply $filename" if $self->verbose;
+
         my $content = $file->slurp;
 
         my $dbh = $self->dbh;
@@ -101,7 +73,7 @@ sub apply_file {
 		my $split_regex = qr/(?!:[\\]);/;
 
 		if ($content =~ m/--\s*split-at:\s*(\S+)\n/s) {
-			warn "Splitting $file commands at >$1<";
+			say "Splitting $filename commands at >$1<";
 			$split_regex = qr/$1/;
 		}
 
@@ -111,19 +83,16 @@ sub apply_file {
             $command =~ s/\s+\Z//;
 
             next unless $command;
-            if ( $self->dry_run ) {
-                say "dry run!" if $self->verbose;
-            }
-            else {
-                say "Executing: $command" if $self->verbose;
-                eval { $dbh->do($command) };
-                if ($@) {
-                    $dbh->rollback;
-                    say "SQL error: $@";
-                    say "ABORTING!";
-                    exit;
-                }
-            }
+            say "Executing SQL statement: $command" if $self->verbose;
+            try {
+                $dbh->do($command)
+            } catch {
+                $dbh->rollback;
+                say "SQL error: $_";
+                say "ABORTING!";
+                exit 1;
+            };
+            say "Successful!" if $self->verbose;
         }
 
         if ( $self->dry_run ) {
@@ -133,9 +102,8 @@ sub apply_file {
         }
 
         # new checksum
-        $self->_schemadump('');
+        $self->reset_checksum;
         my $post_checksum = $self->checksum;
-        $self->dump_checksum($post_checksum) if $self->dump_checksums;
 
         if ( $post_checksum eq $expected_post_checksum ) {
             say "post checksum OK";
@@ -148,11 +116,14 @@ sub apply_file {
             say "  got      $post_checksum";
             $dbh->rollback;
             say "ABORTING!";
-            exit;
+            exit 1;
         }
     }
+    elsif ($answer eq 's') {
+        return $self->apply_sql_snippets($expected_post_checksum);
+    }
     else {
-        say "I am not applying this file. So I stop.";
+        say "Not applying $filename, so we stop.";
         exit;
     }
 }
