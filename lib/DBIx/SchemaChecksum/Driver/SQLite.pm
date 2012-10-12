204a429f69deff26916f90aa8310dbdeb9c8e35a
Maros Kollar <maros@k-1.com>
2012-10-12 11:33:40 +0200

    Fix wrong tablename check

--- lib/DBIx/SchemaChecksum/Driver/SQLite.pm
+++ lib/DBIx/SchemaChecksum/Driver/SQLite.pm
@@ -11,9 +11,9 @@ around '_build_schemadump_table' => sub {
     my ($self,$schema,$table) = @_;
     
     return
-        if $table eq 'sqlite_temp_master' && $schema eq 'temp'
-        || $table eq 'sqlite_sequence' && $schema eq 'main'
-        || $table eq 'sqlite_master' && $schema eq 'main';
+        if ($table eq 'sqlite_temp_master' && $schema eq 'temp')
+        || ($table eq 'sqlite_sequence' && $schema eq 'main')
+        || ($table eq 'sqlite_master' && $schema eq 'main');
         
     return $self->$orig($schema,$table);
 };
