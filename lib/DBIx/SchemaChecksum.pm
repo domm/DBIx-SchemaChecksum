da0019ff29fccbc749541298f78d50df21f95dfa
Thomas Klausner <domm@plix.at>
2012-11-15 17:59:15 +0100

    removed POD now handled by dzil

--- lib/DBIx/SchemaChecksum.pm
+++ lib/DBIx/SchemaChecksum.pm
@@ -378,48 +378,6 @@ q{ Favourite record of the moment: The Dynamics - Version Excursions }
 
 __END__
 
-=head1 AUTHOR
-
-Thomas Klausner, C<< <domm at cpan.org> >>
-Maroš Kollár, C<< <maros at k-1.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to 
-C<bug-dbix-schemachecksum at rt.cpan.org>, or through
-the web interface at 
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBIx-SchemaChecksum>.  
-I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc DBIx::SchemaChecksum
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBIx-SchemaChecksum>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DBIx-SchemaChecksum>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DBIx-SchemaChecksum>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DBIx-SchemaChecksum>
-
-=back
-
 =head1 ACKNOWLEDGEMENTS
 
 Thanks to Klaus Ita and Armin Schreger for writing the core code. I 
@@ -430,14 +388,3 @@ litte software company run by Koki, Domm
 (L<http://search.cpan.org/~domm/>) and Maros 
 (L<http://search.cpan.org/~maros/>).
 
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 - 2012 Thomas Klausner, Maroš Kollár, Klaus Ita, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-The full text of the license can be found in the LICENSE file included
-with this module.
-
-=cut
